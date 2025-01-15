202f7b7846606d61526473d7456113b14e6de663
Eurico Pinto <eurico.mey@gmail.com>
2025-01-15 22:40:28 +0000

    add day-3

---
+++ day-3/day-3.py
@@ -0,0 +1,14 @@
+"""Solution to Day 3 of Advent of Code 2024"""
+
+
+
+def main():
+    print("Day 3 - Advent of Code")
+    
+    # part 1
+
+    # part 2
+
+
+if(__name__=='__main__'):
+    main()
